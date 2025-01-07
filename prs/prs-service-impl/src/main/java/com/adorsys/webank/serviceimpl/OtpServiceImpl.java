cc35a676c355b49fe59cde56b2e33030e1168b21
arielpetit <Ariel.Tchikaya@adorsys.com>
2025-01-07 16:18:16 +0100

    feat(): created the endpoints to send and validate the otp

--- prs/prs-service-impl/src/main/java/com/adorsys/webank/serviceimpl/OtpServiceImpl.java
+++ prs/prs-service-impl/src/main/java/com/adorsys/webank/serviceimpl/OtpServiceImpl.java
@@ -1,4 +1,20 @@
 package com.adorsys.webank.serviceimpl;
 
-public class OtpServiceImpl {
+import com.adorsys.webank.service.OtpServiceApi;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OtpServiceImpl implements OtpServiceApi {
+
+    @Override
+    public String sendOtp(String phoneNumber) {
+        // Implementation will go here
+        return "OTP success";
+    }
+
+    @Override
+    public boolean validateOtp(String phoneNumber, String otp) {
+        // Implementation will go here
+        return false;
+    }
 }
