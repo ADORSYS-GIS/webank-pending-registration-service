7eb7f69b3fde05121f2585c96ac0717f7a96365b
NkwaTambe <nkwajudetambe@gmail.com>
2025-01-07 08:52:41 +0100

    chore:added default classes and main class to start/load application context

--- pending-registration-service-app/src/main/java/com/adorsys/webank/PendingRegistrationServiceApplication.java
+++ pending-registration-service-app/src/main/java/com/adorsys/webank/PendingRegistrationServiceApplication.java
@@ -1,4 +1,11 @@
 package com.adorsys.webank;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class PendingRegistrationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PendingRegistrationServiceApplication.class, args);
+    }
 }
