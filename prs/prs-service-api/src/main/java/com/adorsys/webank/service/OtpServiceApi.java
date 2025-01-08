9100da421bce07a49a4920c0d7cda5615dc8346f
Nancy Muyeh <nancymuyeh@gmail.com>
2025-01-08 10:08:29 +0100

    Implement otp generation mechanism

--- prs/prs-service-api/src/main/java/com/adorsys/webank/service/OtpServiceApi.java
+++ prs/prs-service-api/src/main/java/com/adorsys/webank/service/OtpServiceApi.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface OtpServiceApi {
+    String generateOtp();
     String sendOtp(String phoneNumber);
     boolean validateOtp(String phoneNumber, String otp);
 }
