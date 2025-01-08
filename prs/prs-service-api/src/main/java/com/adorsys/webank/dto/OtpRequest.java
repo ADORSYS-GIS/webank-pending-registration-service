d3190558469c98d66a9fd07f877de04ba30af1ab
Elwizzy12 <chiahans123@gmail.com>
2025-01-08 12:14:46 +0100

    feat:implement logic to hash

--- prs/prs-service-api/src/main/java/com/adorsys/webank/dto/OtpRequest.java
+++ prs/prs-service-api/src/main/java/com/adorsys/webank/dto/OtpRequest.java
@@ -2,18 +2,25 @@ package com.adorsys.webank.dto;
 
 public class OtpRequest {
     private String phoneNumber;
+    private String publicKey;
 
-    public OtpRequest() {}
-
-    public OtpRequest(String phoneNumber) {
+    public OtpRequest(String phoneNumber , String publicKey) {
         this.phoneNumber = phoneNumber;
+        this.publicKey = publicKey;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
+    public String getPublicKey() {
+        return publicKey;
+    }
 
-    public void setPhoneNumber(String phoneNumber) {
+
+    public void setPhoneNumber(String phoneNumber ) {
         this.phoneNumber = phoneNumber;
+    }
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
     }
 }
--- prs/prs-service-api/src/main/java/com/adorsys/webank/service/OtpServiceApi.java
+++ prs/prs-service-api/src/main/java/com/adorsys/webank/service/OtpServiceApi.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 public interface OtpServiceApi {
     String generateOtp();
-    String sendOtp(String phoneNumber);
+    String sendOtp(String phoneNumber, String publicKey);
     boolean validateOtp(String phoneNumber, String otp);
 }
