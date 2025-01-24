0ff5a4c1ae50b1944f56091bb9131ec9dce00959
arielpetit <Ariel.Tchikaya@adorsys.com>
2025-01-24 12:26:57 +0100

    fix(): fix pmd check

--- prs/prs-service-api/src/main/java/com/adorsys/webank/dto/DeviceValidateRequest.java
+++ prs/prs-service-api/src/main/java/com/adorsys/webank/dto/DeviceValidateRequest.java
@@ -1,27 +1,13 @@
 package com.adorsys.webank.dto;
 
+import lombok.Data;
+
+@Data
 public class DeviceValidateRequest {
 
     private String initiationNonce;
     private String powHash;
 
-    public DeviceValidateRequest(String initiationNonce , String powHash) {
-        this.initiationNonce = initiationNonce;
-        this.powHash = powHash;
-    }
-
-    public String getinitiationNonce() {
-        return initiationNonce;
-    }
-    public String powHash() {
-        return powHash;
-    }
 
 
-    public void setinitiationNonce(String initiationNonce ) {
-        this.initiationNonce = DeviceValidateRequest.this.initiationNonce;
-    }
-    public void setpowHash(String powHash) {
-        this.powHash = DeviceValidateRequest.this.powHash;
-    }
 }
