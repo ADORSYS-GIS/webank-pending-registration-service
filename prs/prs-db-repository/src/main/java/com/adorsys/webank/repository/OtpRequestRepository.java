a4bbf65b631f0ee40e89df000b5e91376c2cdc2d
NkwaTambe <nkwajudetambe@gmail.com>
2025-03-13 15:45:23 +0100

    fix pmd check violations

--- prs/prs-db-repository/src/main/java/com/adorsys/webank/repository/OtpRequestRepository.java
+++ prs/prs-db-repository/src/main/java/com/adorsys/webank/repository/OtpRequestRepository.java
@@ -4,8 +4,9 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
+import com.adorsys.webank.domain.OtpRequest;
 
 @Repository
-public interface OtpRequestRepository extends JpaRepository<com.adorsys.webank.domain.OtpRequest, UUID> {
+public interface OtpRequestRepository extends JpaRepository<OtpRequest, UUID> {
     Optional<OtpRequest> findByPublicKeyHash(String publicKeyHash);
 }
