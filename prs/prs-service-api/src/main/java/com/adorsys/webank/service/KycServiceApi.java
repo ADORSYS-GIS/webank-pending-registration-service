439cbb07535f8383c4e76ae0b0546dd905fde51d
arielpetit <Ariel.Tchikaya@adorsys.com>
2025-03-18 16:51:56 +0100

    Chore(): merged kyc related BE tickets

--- prs/prs-service-api/src/main/java/com/adorsys/webank/service/KycServiceApi.java
+++ prs/prs-service-api/src/main/java/com/adorsys/webank/service/KycServiceApi.java
@@ -1,4 +1,7 @@
 package com.adorsys.webank.service;
+import com.adorsys.webank.domain.PersonalInfoEntity;
+import com.adorsys.webank.domain.PersonalInfoStatus;
+import com.adorsys.webank.domain.UserDocumentsEntity;
 import com.adorsys.webank.dto.KycDocumentRequest;
 import com.adorsys.webank.dto.KycEmailRequest;
 import com.adorsys.webank.dto.KycInfoRequest;
@@ -6,10 +9,16 @@ import com.adorsys.webank.dto.KycLocationRequest;
 import com.nimbusds.jose.jwk.JWK;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public interface KycServiceApi {
     String sendKycDocument(JWK devicePub, KycDocumentRequest kycDocumentRequest);
     String sendKycinfo(JWK devicePub, KycInfoRequest kycInfoRequest);
     String sendKyclocation(JWK devicePub, KycLocationRequest kycLocationRequest);
     String sendKycEmail(JWK devicePub, KycEmailRequest kycEmailRequest);
+    Optional<UserDocumentsEntity> getDocuments(String publicKeyHash);
+    Optional<PersonalInfoEntity> getPersonalInfoByPublicKey(String publicKeyHash);
+    List<PersonalInfoEntity> getPersonalInfoByStatus(PersonalInfoStatus status);
 }
