94d5e5c5300f3d29f921d6a464783f22b0bf255b
Menkene Koufan <Menkene.Koufan@adorsys.com>
2025-06-09 12:44:52 +0100

    used annotation for constructor rather than boilerplate

--- prs/prs-service-api/src/main/java/com/adorsys/webank/dto/EmailOtpValidationRequest.java
+++ prs/prs-service-api/src/main/java/com/adorsys/webank/dto/EmailOtpValidationRequest.java
@@ -1,19 +1,13 @@
 package com.adorsys.webank.dto;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-
-
+@RequiredArgsConstructor
 public class EmailOtpValidationRequest {
     private String email;
     private String otpInput;
     private String accountId;
-
-    public EmailOtpValidationRequest(String email, String otpInput, String accountId) {
-        this.email = email;
-        this.otpInput = otpInput;
-        this.accountId = accountId;
-    }
 }
